391496eb5d521e9fcfdce57f1b1526a0d0469262
Caitlin Bender <85584674+coolitnow@users.noreply.github.com>
2022-01-23 16:14:31 -0500

    Add <router-outlet> to the AppComponent template which will tell the router were to display routed views.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+//necessary for routing functionality
 import { RouterModule, Routes } from '@angular/router';
+//gives router somewhere to go once configured
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
-  { path: 'heroes, component: HeroesComponent'}
+  { path: 'heroes', component: HeroesComponent}
 ];
 
 @NgModule({
