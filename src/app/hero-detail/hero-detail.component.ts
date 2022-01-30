27253c3ec47a34f75d3e7e9e1198d414f297f0c8
Caitlin Bender <85584674+coolitnow@users.noreply.github.com>
2022-01-30 16:31:54 -0500

    Extract id route parameter in the Hero-Detail component. Add getHero() method to HeroService to grab id.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 
 //child component of HeroesComponent
@@ -13,9 +16,22 @@ export class HeroDetailComponent implements OnInit {
 
   //@Input() decorator makes a property available for binding by external components
   @Input() hero?: Hero;
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute, 
+    //holds info about the route to this instance of HeroDetailComponent
+    private heroService: HeroService,
+    //gets hero data from remote server and will use it to get the hero to display
+    private location: Location,
+    //an Angular service for interacting with the browser
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
 }
